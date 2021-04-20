f6686cda5160d5d6afae1c7aaf5b15c8e2ce9a3b
Arnas Gabijunas <arnasg@stud.ntnu.no>
2021-04-20 13:33:53 +0000

    Update controller/APIController.php, db/CarModel.php, setup/.gitignore, setup/.htaccess, setup/api.suite.yml, setup/dbCredentialsTemplate.php, setup/read.me, setup/setupCodeception.bat, setup/testdb.sql, setup/unit.suite.yml, tests/_data/.gitkeep, tests/_data/testdb.sql, tests/_support/AcceptanceTester.php, tests/_support/ApiTester.php, tests/_support/FunctionalTester.php, tests/_support/UnitTester.php, tests/_support/Helper/Acceptance.php, tests/_support/Helper/Api.php, tests/_support/Helper/Functional.php, tests/_support/Helper/Unit.php, tests/_support/_generated/AcceptanceTesterActions.php, tests/_support/_generated/FunctionalTesterActions.php, tests/_support/_generated/ApiTesterActions.php, tests/_support/_generated/UnitTesterActions.php, tests/api/yaAPICest.php, tests/unit/controllerTest.php, .gitignore, api.php, RESTConstants.php files

---
+++ db/CarModel.php
@@ -0,0 +1,90 @@
+<?php
+require_once 'dbCredentials.php';
+
+class CarModel {
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',
+            DB_USER, DB_PWD,
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+
+    /**
+     * Returns the collection of resources from the database.
+     * @param array $query an optional set of conditions that the retrieved
+     *              resources need to meet - e.g., array('make' => 'Ford') would
+     *              mean that only resources having make = Ford would be returned.
+     * @return array an array of associative arrays of resource attributes. The
+     *               array will be empty if there are no resources to be returned.
+     * @throws APIException if the $query variable is incorrectly formatted
+     */
+    function getCollection(array $query = null): array
+    {
+        $res = array();
+
+        return $res;
+    }
+
+    /**
+     * Returns the collection of resources from the database.
+     * @param int $id the id of the resource to be retrieved.
+     * @return array an associative array of resource attributes - or null if
+     *               no resources have the given id.
+     */
+    function getResource(int $id): array
+    {
+        $res = array();
+        $query = 'SELECT id, make, model, model_year, mileage, fuel, type, price, dealer_id, comment FROM car WHERE id = :id';
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            return $row;
+        }
+        return $res;
+    }
+
+    /**
+     * Creates a new resource in the database.
+     * @param array $resource the resource to be created.
+     * @return array an associative array of resource attributes representing
+     *               the resource - the returned value will include the id
+     *               assigned to the resource.
+     */
+    function createResource(array $resource): array
+    {
+        $res = array();
+
+        return $res;
+    }
+
+    /**
+     * Modifies a resource in the database.
+     * @param array $resource the resource to be modified.
+     * @return array an associative array of resource attributes representing
+     *               the resource after being modified.
+     * @throws APIException if the $resource is missing some of the required
+     *                      attributes.
+     */
+    function modifyResource(array $resource): array
+    {
+        $res = array();
+
+        return $res;
+    }
+
+    /**
+     * Deletes a resource from the database.
+     * @param int $id the id of the resource to be deleted.
+     * @throws APIException if the record cannot be deleted from the database,
+     *                      e.g., due to foreign key constraints.
+     */
+    function deleteResource(int $id)
+    {
+
+    }
+}
---
+++ setup/dbCredentialsTemplate.php
@@ -0,0 +1,5 @@
+<?php
+const DB_HOST = '127.0.0.1';
+const DB_NAME = 'demotest';
+const DB_USER = 'aUser';
+const DB_PWD = '********';
