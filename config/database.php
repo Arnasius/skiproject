9e557c5a8061f7f81126523907cc4eec5c2fcfc9
ArnasG <arnasg@ntnu.no>
2021-05-22 18:09:31 +0200

    DB connection done (formatting is ugly)

--- config/database.php
+++ config/database.php
@@ -2,18 +2,38 @@
 require_once 'dbCredentials.php';
 
 // Initializing a database connection
-    class Database {
-        public $conn;
+    abstract class DBASE
+    {
+        /**        public $conn;
+         *
+         * public function getConnection(){
+         * $this->conn = null;
+         * try{
+         * $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PWD);
+         *
+         * }
+         * catch(PDOException $exception){
+         * echo "Database could not be connect: " . $exception->getMessage();
+         * }
+         * return $this->conn;
+         * }
+         * }
+         */
+        protected $conn;
 
-        public function getConnection(){
+        public function __construct()
+        {
             $this->conn = null;
-            try{
-                $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USER,DB_PWD);
 
-            }
-            catch(PDOException $exception){
-                echo "Database could not be connect: " . $exception->getMessage();
-            }
-            return $this->conn;
+            try{
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PWD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+
+        }
+        catch (PDOException $exception)
+        {
+        echo "database could not be connected: " . $exception->getMessage();
+        }
+        return $this->conn;
         }
     }
